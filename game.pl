99016737138fe8edff3704bc993d25c575e3c565
João Cabral <joaofigueiredocabral@gmail.com>
2015-11-05 18:10:19 +0000

    misterios da criaçao

--- game.pl
+++ game.pl
@@ -16,7 +16,7 @@ piece(flagship).
 player(goldenPlayer).
 player(silverPlayer).
 
-
+currentPlayer(goldenPlayer).
 
 flagships(1).
 
@@ -29,117 +29,65 @@ state(begin).
 state(game).
 state(over).
 
-teste(_):- createBoard(3,3,Board),
-		replaceInBoard(2,2, boda, Board, NewBoard),
-		getCell(NewBoard, 2, 2, boda).
 
-startGame:- assert(goldenPieces(0)), assert(silverPieces(0)),
-			createBoard(11,11,Board),
-			replaceInBoard(5, 5, flagship, Board, NewBoard),
-			setupBoard(NewBoard, NewerBoard),
-			playGame(NewerBoard).
+startGame:- retractall(position(_,_,_)), assert(goldenPieces(0)), assert(silverPieces(0)),
+	setupBoard,
+	playGame.
 
-playGame(Board):-
+playGame:-
 	readPlayer(Player),
-	assert(gameState(Board, Player)),
+	assert(player(Player)),
 	repeat,
-	retract(gameState(CurrentBoard, CurrentPlayer)),
-	takeTurn(Player, CurrentBoard, NewBoard, NewPlayer),
-	printBoard(NewBoard, 11),
-	assert(gameState(NewBoard, Player)),
-	fail. %mudar para winning condition
+	retract(player(NPlayer)),
+	takeTurn(NPlayer, CurrentBoard, NewBoard, NewPlayer),
+	printBoard(0, 0),
+	assert(player(NewPlayer)),
+	fail.
 
 
 
-setupBoard(Board, NewBoard):- assert(gameState(Board, goldenPlayer)),
-					fillBoard(11,11),
-					repeat,
-					retract(gameState(CurrentBoard, CurrentPlayer)),
-					write(CurrentPlayer),
-					placePiece(CurrentBoard, CurrentPlayer, NewBoard, NewPlayer),
-					printBoard(NewBoard, 11),
-					assert(gameState(NewBoard, NewPlayer)),
-					boardFull.
+setupBoard:-
+	fillBoard,
+	asserta(position(5,5,flagship)),
+	placePiece(goldenPlayer, 0).
+
+placePiece(goldenPlayer, 4):- placePiece(silverPlayer, 0).
+placePiece(goldenPlayer, N):- N1 is N+1, N < 4,
+	write('New piece for golden player:'), nl,
+	repeat,
+	readCoordinates(X,Y),
+	validateCoordinates(X,Y, goldenPlayer),!,
+	asserta(position(X,Y,goldenPiece)),
+		printBoard,
+	placePiece(goldenPlayer, N1).
+
+placePiece(silverPlayer, 4).
+placePiece(silverPlayer, N):- N1 is N+1, N< 4,
+	write('New piece for silver player:'), nl,
+	repeat,
+	readCoordinates(X,Y),
+	validateCoordinates(X,Y, silverPlayer),!,
+	asserta(position(X,Y,silverPiece)),
+	placePiece(silverPiece, N1).
 
 
 boardFull:- goldenPieces(N), flagships(F), silverPieces(J),
-			J == 4, %devia ser 20
-			N == 4, %devia ser 11
-			F == 1. %taboum
-
-placePiece(CurrentBoard, goldenPlayer, NewBoard, goldenPlayer):-
-goldenPieces(J), J < 3,
- retract(goldenPieces(G)),
- write('New Piece Coordinates: '),
- repeat,
- readCoordinates(X, Y),
- validateCoordinates(CurrentBoard, X, Y, goldenPlayer),
- replaceInBoard(X, Y, goldenPiece, CurrentBoard, NewBoard),
- asserta( (position(X,Y, goldenPiece)) ),
- G1 is G+1,
- assert(goldenPieces(G1)),
- !.
-
- placePiece(CurrentBoard, goldenPlayer, NewBoard, silverPlayer):-
- retract(goldenPieces(G)),
- write('New Piece Coordinates: '),
- repeat,
- readCoordinates(X, Y),
- validateCoordinates(CurrentBoard, X, Y, goldenPlayer),
- replaceInBoard(X, Y, goldenPiece, CurrentBoard, NewBoard),
- asserta( (position(X,Y,goldenPiece)) ),
- G1 is G+1,
- assert(goldenPieces(G1)),
- !.
+	J == 4, %devia ser 20
+	N == 4, %devia ser 11
+	F == 1. %taboum
 
 
- placePiece(CurrentBoard, silverPlayer, NewBoard, silverPlayer):-
- silverPieces(J), J =< 3, %aqui e 20
-  retract(silverPieces(G)),
- write('New Piece Coordinates: '),
- repeat,
- readCoordinates(X, Y),
- validateCoordinates(CurrentBoard, X, Y,  silverPlayer),
- replaceInBoard(X, Y, silverPiece, CurrentBoard, NewBoard),
- asserta( (position(X,Y, silverPiece)) ),
- G1 is G+1,
- assert(silverPieces(G1)),
- !.
-
-
-
-validateCoordinates(Board, X, Y, goldenPlayer):-
+validateCoordinates( X, Y, goldenPlayer):-
 	X > 2, X < 8,
 	Y > 2, Y < 8,
-	getCell(Board, X, Y, emptyCell), !.
+	findall(Z, position(X,Y,Z), [emptyCell]).
 
-validateCoordinates(Board, X, Y, silverPlayer):-
-	\+(validateCoordinates(Board, X, Y, goldenPlayer)),
-	getCell(Board, X, Y, emptyCell), !.
-
-
-takeTurn(Player, Board, NewBoard, NewPlayer):-
-	write('Turn for '), write(Player), nl,
-	write('Choose the piece you want to move: '), nl,
-	readCoordinates(XI, YI),
-	write('Where do you want to move it to: '), nl,
-	readCoordinates(XF, YF),
-	evaluateMove(XI, YI, XF, YF, Board, NewBoard, Player).
+validateCoordinates(X, Y, silverPlayer):-
+	\+(validateCoordinates( X, Y, goldenPlayer)),
+	findall(Z, position(X,Y,Z), [emptyCell]).
 
 
 
-evaluateMove(Xi, Yi, Xf, Yf, Board, NewererBoard, Player):-
-	getCell(Board, Xi, Yi, Piece),
-	owner(Player, Piece),
-	calculateDistances(Xi, Yi, Xf, Yf, DX, DY),
-	(DX \= 0, DY is 0;
-	DX is 0, DY \= 0;
-	DX is 1, DY is 1;
-	DX is 1, DY is -1;
-	DX is -1, DY is 1;
-	DX is -1, DY is -1),
-	replaceInBoard(Xi, Yi, emptyCell, Board, NewBoard),
-	replaceInBoard(Xf, Yf, Piece, NewBoard, NewererBoard).
 
 calculateDistances(Xi, Yi, Xf, Yf, DX, DY):-
 	DX is Xf - Xi,
