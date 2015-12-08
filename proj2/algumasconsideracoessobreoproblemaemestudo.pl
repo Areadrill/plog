37563b912c4e802b87c1b69c636d3e50248f4ddd
João Cabral <joaofigueiredocabral@gmail.com>
2015-12-08 23:10:11 +0000

    restriçao 15 horas semanais/professor

--- proj2/algumasconsideracoessobreoproblemaemestudo.pl
+++ proj2/algumasconsideracoessobreoproblemaemestudo.pl
@@ -36,7 +36,8 @@ testeProfs(Sol):-
 
 initialize_days_list([],_,_,_).
 initialize_days_list([H|T], N, NProfessores, FdSetDeProfs):-
-	H = N-Hora-Prof,
+	H = [N,Hora,Prof],
+
 	Prof in_set FdSetDeProfs,
 	domain([Hora], 0, 4),
 	N1 is N+1,
@@ -68,8 +69,43 @@ excludeEmpty([Lingua-Horarios|Solucoes], Candidaturas):-
 	excludeEmpty([Lingua-Horarios|Solucoes], Candidaturas):-
 		excludeEmpty(Solucoes, Candidaturas).
 
+
+% [ 0-[ [0, 2, 4], [1, 3, 5] ], 1-[[0,4,5], [1,3,5],[2,6,6] ]  ]
+
 testeExclude:-
 	excludeEmpty([0-[]|T], [0-0]).
+labelAll([]).
+labelAll([H|T]):-
+	H = _-[Dia1,Dia2,Dia3,Dia4,Dia5],
+	labeling([], Dia1),
+	labeling([], Dia2),
+	labeling([], Dia3),
+	labeling([], Dia4),
+	labeling([], Dia5),
+	labelAll(T).
+
+restrict_teacher_hours(Solucao, []).
+restrict_teacher_hours(Solucao, [Professor|T]):-
+	Professor=[Indice,_,_],
+	restrict_teacher(Solucao, Indice, 0),
+	restrict_teacher_hours(Solucao, T).
+
+%
+restrict_teacher([], _, Acum):- Acum #< 15, write(Acum).
+restrict_teacher([H|T], Indice, Acum):-
+	H = IndiceL-Dias,
+	teacher_times(Dias, Indice, Time),
+	Acum1 #= Time+Acum,
+	restrict_teacher(T, Indice, Acum1).
+
+
+teacher_times([], Indice, 0).
+teacher_times([[IndiceDia, Horas, ProfessorResponsavel]|Dias], Indice, Time):-
+	ProfessorResponsavel #= Indice #<=> B,
+	TempoQueOProfDaEstaAula #= B*Horas,
+	teacher_times(Dias, Indice, TempoRestante),
+	Time #= TempoQueOProfDaEstaAula+TempoRestante.
+
 
 
 
@@ -79,10 +115,45 @@ escola_de_linguas(Professores, Candidaturas, Linguas, Lucro, Solucao):-
 	length(Linguas, NLinguas),
 	length(Solucao, NLinguas),
 	initialize_solution(Solucao, 0, NProfessores, Professores),
-	excludeEmpty(Solucao, Candidaturas).
-
-
+	restrict_teacher_hours(Solucao, Professores),
+	labelAll(Solucao).
 
 teste(Solucao):-
-	escola_de_linguas([[0, [0-19, 1-19, 2-10],[0,1,2,3,4,5,6]], [1, [4-25],[0,1,2]]], [0-15, 1-10, 2-3, 3-10,4-0], [0-1,1-1,2-2,3-3,4-4], _, Solucao).
-%	escola_de_linguas([[0, [0-10],[0,1]]], [0-0], [0-20], _, Solucao).
+%	escola_de_linguas([[0, [0-19, 1-19, 2-10],[0,1,2,3,4,5,6]], [1, [4-25],[0,1,2]]], [0-15, 1-10, 2-3, 3-10,4-1], [0-1,1-1,2-2,3-3,4-4], _, Solucao).
+	escola_de_linguas([[0, [0-10],[0,1]]], [0-10], [0-20], _, Solucao).
+
+testinho(P,0,0).
+testinho(P, N, Value):-
+	N1 is N-1,
+	testinho(P, N1, Value1),
+	Value #= P+Value1.
+
+make_profit([], _, _, 0).
+make_profit([Solucao|Solucoes], Linguas, Professores, Candidaturas, Lucro):-
+	make_profit_language(Solucao, Linguas, Professores, Candidaturas, LucroLingua),
+	make_profit(Solucoes, Linugas, Professores, Candidaturas, LucroResto),
+	Lucro is LucroResto+LucroLingua.
+
+%Lingua = Indice-[[0, 1,1],[1,2,3],[3,4,5]|...]
+make_profit_language([], _,_,0).
+make_profit_language([Lingua|OutrasLinguas], Linguas, Professores, Candidaturas, LucroLingua):-
+	Lingua = IndiceL-Dias,
+	make_profit_day(Dias, Linguas, Professores, Candidaturas, IndiceL, LucroDia),
+	LucroLingua is LucroDia.
+
+make_profit_day([Dia|OutrosDias], Linguas, Professores, Candidaturas, IndiceL, LucroDia):-
+	Dia = [IndiceDia, Horas, Professor],
+	member([Professor, LinguasProfessor, _], Professores),
+	member(IndiceL-Remuneracao, LinguasProfessor),
+	Prejuizo is Remunaracao * Horas,
+	member(IndiceL-Preco, Linguas),
+	member(IndiceL-NumeroCandidatos, Candidaturas),
+	Lucro is Preco*Candidaturas*Horas,
+	LucroDia is Lucro-Prejuizo.
+
+
+
+
+
+%   0-[[0,_662339,_662259],[1,_662647,_662567],[2,_662955,_662875],[3,_663263,_663183],[4,_663571,_663491]]
+%
