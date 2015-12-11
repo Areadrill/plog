5e25634e4e86a23fcd9018f86c44ac209a4165dc
João Cabral <joaofigueiredocabral@gmail.com>
2015-12-11 04:27:48 +0000

    ITS A MIRACLE

--- proj2/algumasconsideracoessobreoproblemaemestudo.pl
+++ proj2/algumasconsideracoessobreoproblemaemestudo.pl
@@ -35,28 +35,50 @@ professoresQueDaoLingua(Lingua,  [ [_, _,_] | OutrosProfs], Lista):-
 testeProfs(Sol):-
 	professoresQueDaoLingua(5, [[0, [0-1,1-2],_], [1, [0-2],_], [2, [3-2],_]], Sol).
 
+generate_professor_availability([], []).
+generate_professor_availability([[Indice,_,Dias]|Professors], Table):-
+	generate_professor_availability(Dias,Indice,TableThis),
+	generate_professor_availability(Professors, TableRest),
+	append(TableThis, TableRest, TableN),
+	append([[-1,0],[-1,1],[-1,2],[-1,3],[-1,4],[-1,5]], TableN, Table).
+
+generate_professor_availability([], _, []).
+generate_professor_availability([Dia|Dias],Indice, Table):-
+	generate_professor_availability(Dias, Indice, TableRest),
+	append([[Indice,Dia]], TableRest, Table).
 
 
 
-initialize_days_list([],_,_,_).
-initialize_days_list([H|T], N, NProfessores, FdSetDeProfs):-
+
+
+initialize_days_list([],_,_,_,_).
+initialize_days_list([H|T], N, NProfessores, FdSetDeProfs, ProfAvailable):-
 	H = [N,Hora,Prof],
-
-	Prof in_set FdSetDeProfs,
+	Prof #= -1 #=> Hora #= 0,
+	table([[Prof,N]],ProfAvailable),
+	Prof in_set FdSetDeProfs ,
 	domain([Hora], 0, 4),
 	N1 is N+1,
-	initialize_days_list(T, N1, NProfessores, FdSetDeProfs).
+	initialize_days_list(T, N1, NProfessores, FdSetDeProfs, ProfAvailable).
 
 
-initialize_solution([], _,_,_).
-initialize_solution([H|T], Lingua, NProfessores, Professores):-
+
+
+initialize_solution([], _,_,_,_).
+initialize_solution([H|T], Lingua, NProfessores, Professores, ProfAvailable):-
 	H = Lingua-Lista,
 	length(Lista, 5),
 	professoresQueDaoLingua(Lingua, Professores, ListaDeProfs),
 	list_to_fdset([-1|ListaDeProfs], FdSetDeProfs),
-	initialize_days_list(Lista, 0, NProfessores,FdSetDeProfs),
+	initialize_days_list(Lista, 0, NProfessores,FdSetDeProfs,ProfAvailable),
 	Lingua1 is Lingua+1,
-	initialize_solution(T, Lingua1, NProfessores, Professores).
+	initialize_solution(T, Lingua1, NProfessores, Professores, ProfAvailable).
+
+	restrict_course_times([]).
+	restrict_course_times([_-[[_,H1,_],[_,H2,_],[_,H3,_],[_,H4,_],[_,H5,_]]|T]):-
+		sum([H1,H2,H3,H4,H5],#=<,4),
+		sum([H1,H2,H3,H4,H5],#>=,2),
+		restrict_course_times(T).
 
 forceEmpty([]).
 forceEmpty([_-Horas-Prof|T]):-
@@ -80,9 +102,8 @@ labelAll([],_).
 labelAll(Sol, Cost):-
 	labelAll(Sol, Cost, []).
 
-labelAll([], Cost, Flat):-write('gona label'),nl, labeling([maximize(Cost)], Flat).
+labelAll([], Cost, Flat):-write('gona label'),nl, labeling([ff,enum,down,maximize(Cost)], Flat).
 labelAll([_-[Dia1,Dia2,Dia3,Dia4,Dia5]|Tail], Cost, Flat):-
-	write('labeling'),nl,
 	append(Flat, Dia1, Flat1),
 	append(Flat1, Dia2, Flat2),
 	append(Flat2, Dia3, Flat3),
@@ -90,37 +111,73 @@ labelAll([_-[Dia1,Dia2,Dia3,Dia4,Dia5]|Tail], Cost, Flat):-
 	append(Flat4, Dia5, Flat5),
 	labelAll(Tail, Cost, Flat5).
 
-restrict_teacher_hours(_, []).
-restrict_teacher_hours(Solucao, [Professor|T]):-
+restrict_teacher_hours(_,_,[], _).
+restrict_teacher_hours(Horas, ProfessoresDominio, [[Indice,_,_]|T], NProfs):-
+	make_prof_table(Indice, Table, NProfs),!,
+	restrict_teacher(Table, ProfessoresDominio, Rest),
+	sum_product(Horas, Rest, Sum),
+	Sum #=< 15,
+	restrict_teacher_hours(Horas, ProfessoresDominio, T, NProfs).
 
-	
+restrict_teacher(_, [], []).
+restrict_teacher(Table, [-1|Ps],[0|T]):- restrict_teacher(Table, Ps, T).
+restrict_teacher(Table, [P|Ps], [Rest|T]):-
+	table([[P,Rest]], Table),
+	restrict_teacher(Table, Ps, T).
+
+
+sum_product([],[],0).
+sum_product([X|Xs],[Y|Ys], Sum):-
+	sum_product(Xs,Ys,SumRest),
+	Sum #= X*Y + SumRest.
+
+
+
+
+
+
+
 generate_domain_day_list([],[],[]).
 generate_domain_day_list([[_,Hora,Prof]|Dias], H, P):-
 	generate_domain_day_list(Dias, HRest, PRest),
 	append([Hora], HRest, H),
 	append([Prof], PRest, P).
-	
+
 generate_variable_domain_list([],[],[]).
 generate_variable_domain_list([_-Dias|Solucoes], H, P):-
 	generate_domain_day_list(Dias, HThis, PThis),
 	generate_variable_domain_list(Solucoes, HRest, PRest),
 	append(HThis, HRest, H),
-	append(PThis, PRest, P).	
-	
+	append(PThis, PRest, P).
+
+custo_nulo([], 0).
+custo_nulo(TabelaCustos, NLinguas):-
+	NLinguas \= 0,
+	write(NLinguas),
+	N is NLinguas-1,
+	custo_nulo(TabelaCustosR, N),
+	append([[-1,N,0]], TabelaCustosR, TabelaCustos).
+
 escola_de_linguas(Professores, Candidaturas, Linguas, Lucro, Solucao):-
 	length(Professores, NProfessores),
 	length(Linguas, NLinguas),
 	length(Solucao, NLinguas),
-	cria_tabela_custos(Professores, TabelaCustos, []),
-	initialize_solution(Solucao, 0, NProfessores, Professores),
-	generate_variable_domain_list(Solucao, H, P),write(P), nl , write(H), nl,
-	%restrict_teacher_hours(Solucao, Professores),
+	generate_professor_availability(Professores, ProfAvailable),
+	cria_tabela_custos(Professores, TabelaCustosReais, []),
+	write(NLinguas), nl, nl,
+	custo_nulo(CustoNulo, NLinguas),
+	append(CustoNulo, TabelaCustosReais, TabelaCustos),write(TabelaCustos),
+	initialize_solution(Solucao, 0, NProfessores, Professores,ProfAvailable),
+	generate_variable_domain_list(Solucao, H, P),
+	restrict_teacher_hours(H, P, Professores, NProfessores),
 	restrict_course_times(Solucao),
 	make_profit(Solucao, Linguas, Professores, TabelaCustos, Candidaturas, Lucro),
+
 	write(Lucro),
-	labelAll(Solucao, Lucro).
-	
-	
+	labelAll(Solucao, Lucro),
+	fd_statistics.
+
+
 make_prof_table(Indice, Lista, NProfs):-
 	Length is NProfs+1,
 	length(Lista, Length),
@@ -129,11 +186,15 @@ make_prof_table(Indice, Lista, NProfs):-
 iterate_prof_table(_,-1,_).
 iterate_prof_table(Lista, NProfs, NProfs):- !, nth0(NProfs, Lista, [NProfs, 1]), Next is NProfs-1, iterate_prof_table(Lista, Next, NProfs).
 iterate_prof_table(Lista, NProfs, Indice):- nth0(NProfs, Lista, [NProfs,0]), Next is NProfs-1, iterate_prof_table(Lista, Next, Indice).
-	
-	
+
+
 teste(Lucro, Solucao):-
-%escola_de_linguas([[0, [0-19, 1-19, 2-10,3-4,4-5],[0,1,2,3,4,5,6]], [1, [4-25],[0,1,2]]], [0-15, 1-10, 2-3, 3-10,4-1], [0-1,1-1,2-2,3-3,4-4], Lucro, Solucao).
-escola_de_linguas([[0, [0-10,1-10],[0,1]], [1, [0-5],[0,1]],[2, [0-5],[0,1]]], [0-10,1-10], [0-20,1-40], Lucro, Solucao).
+	statistics(runtime, [T0| _]),
+	escola_de_linguas([[0, [0-10,1-10],[0,1]], [1, [0-5,2-1],[0,1]],[2, [0-5],[0,1]]], [0-10,1-10,2-10], [0-20,1-40,2-10], Lucro, Solucao),
+	%escola_de_linguas([[0, [0-1, 1-1,2-1],[0,2,3,4,5,6]]], [0-15, 1-10,2-10,3-10], [0-1,1-1,2-1], Lucro, Solucao),
+	statistics(runtime, [T1|_]),
+	T is T1 - T0,
+	format('p/0 took ~3d sec.~n', [T]).
 
 
 cria_tabela_custos([], TabelaCustos, TabelaCustos).
@@ -142,14 +203,14 @@ cria_tabela_custos([Professor|Professores], TabelaCustos, Acum):-
 	cria_tabela_linguas(Linguas, Indice, TabelaLinguas, []),
 	append(Acum, TabelaLinguas, NovaTabela),
 	cria_tabela_custos(Professores, TabelaCustos, NovaTabela).
-	
-	
+
+
 cria_tabela_linguas([], _, T, T).
 cria_tabela_linguas([LinguaCusto|LinguasCustos], Indice, TabelaLinguas, Acum):-
 	LinguaCusto = IndiceL-Custo,
 	append(Acum, [[Indice,IndiceL,Custo]], NovaTabela),
 	cria_tabela_linguas(LinguasCustos, Indice, TabelaLinguas, NovaTabela).
-	
+
 make_profit([], _, _, _,_, 0).
 make_profit([Solucao|Solucoes], Linguas, Professores,TabelaCustos, Candidaturas, Lucro):-
 	make_profit_language(Solucao, Linguas, Professores, TabelaCustos, Candidaturas, LucroLingua),
@@ -167,6 +228,7 @@ make_profit_day([Dia|OutrosDias], Linguas, Professores, TabelaCustos, Candidatur
 	Prejuizo #= Remuneracao * Horas,
 	member(IndiceL-Preco, Linguas),
 	member(IndiceL-NumeroCandidatos, Candidaturas),
+
 	Lucro #= Preco*NumeroCandidatos*Horas,
 	LucroDiaAtual #= (Lucro-Prejuizo),
 	make_profit_day(OutrosDias, Linguas, Professores,TabelaCustos, Candidaturas, IndiceL, LucroRest),
